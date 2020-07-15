0dd9aca3365752af3b8bc39212fd5e5aedb8ea7b
agiletkiewicz <a.letkiewicz@gmail.com>
2020-07-15 15:13:38 -0700

    Add helper methods for login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,17 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
+  helpers do 
+
+    def current_user 
+      User.find_by(id: session[:user_id])
+    end
+
+    def logged_in?
+      !!current_user
+    end
+
+  end
+
+
 end
