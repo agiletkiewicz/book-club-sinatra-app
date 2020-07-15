2a1e21dc7e7adc10729707f046e203b0ce58dc0d
agiletkiewicz <a.letkiewicz@gmail.com>
2020-07-15 14:26:27 -0700

    Add user sign-up validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
 
+    validates :username, uniqueness: true
+    validates :name, :username, :password, :bio, presence: true
+
     has_many :likes
     has_many :books, through: :likes
 
